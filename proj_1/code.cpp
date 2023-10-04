97138e744b692273d07b949f641bee8719c7984d
bigdog224 <lamfamily789@gmail.com>
2023-10-04 13:19:40 -0400

    part 1 changes

--- proj_1/code.cpp
+++ proj_1/code.cpp
@@ -1,38 +1,99 @@
 #include "code.h"
 #include <iostream>
+#include <string>
+#include <unordered_map>
 using namespace std;
+
+
+code::code(string x) {
+    try{
+        int guess = stoi(x);
+        
+        if (guess >= 0) {
+            for(int i = 0; i < ; i++) {
+                secret.push_back(guess[i]);
+            }
+        } else {
+            throw bad_cast;
+        }
+    }
+    catch (bad_cast) {
+        return "Please input positive numbers.";
+    }
+};
 
 code::code(int n, int m){
     // n = number of digits
-    // m = range of digits (0-1)
-    size = m;
-    for (int i=0; i<n;i++){
-        int randomNumber = getRandomNumber(m);
-        secret.push_back(randomNumber)
+    // m = range of digits from 0 - (m-1)
+
+    try {
+        if (typeid(n).name() == "i" && typeid(m).name() == "i") {
+            int size = n
+
+            for (int i = 0; i < n; i++){
+                int randomNumber = getRandomNumber(m);
+                secret.push_back(randomNumber);
+                if (counter.find(randomNumber) == counter.end()) {
+                    counter[randNumber] += 1;
+                } else {
+                    counter[randNumber] = 1;
+                }
+            }
+        } else {
+            throw 505;
+        }
+    } catch (...) {
+        return "Please give integer inputs for the first and second argument in the constructor";
     }
 };
 
 int code::checkCorrect(const code& guess){
     // Check how many guesses have the correct number in an correct spot 
     int correct = 0;
-    if (size != giess.getSize){
+    if (size != guess.getSize()){
         cout << "Invalid Guess" << endl;
-        return
+        return;
     }
-    for(int i=0;i<m;i++) {
+    for(int i = 0;i < size;i++) {
         if (secret[i] == guess.getNumberByIndex(i)){
-            count += 1
+            correct += 1;
         }
     }
-    return count
+    return correct;
 };
 
 int code::checkIncorrect(const code& guess){
-
     // Check how many guesses have the correct number but in an incorrect spot
+        int incorrect = 0;
+        unordered_map<int, int> guess_count = guess.getCounter();
+
+        for(const auto& pair: guess_count) {
+
+            incorrect += min(pair.second, counter[pair.first]);
+        }
+
+        return incorrect;
 };
 
 int code::getRandomNumber(int range){
     // Get a random number within [0, range-1]
-    return rand() % range 
-}
+    return rand() % range;
+};
+
+void code::play(){
+
+};
+
+int main() {
+    string x, y;
+    int game_over = 0;
+
+    cout << "Welcome to Mastermind! Give the length of the code you want to guess along with the range for each number in the code!" << endl;
+        
+    cout << "Please input the length of the code: ";
+    cin >> x;
+    cout << "Please input the range of each number in the code: ";
+    cin >> y;
+    code secret = code(x, y);
+};
+
--- proj_1/code.h
+++ proj_1/code.h
@@ -2,17 +2,23 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 class code {
     public:
+        code(string x);
         code(int n, int m);
         int checkCorrect(const code& guess) const;
         int checkIncorrect(const code& guess) const;
         int getSize(){return size};
-        int getNumberByIndex(){return vector[i]}
+        int getCount() {return guessCount};
+        int getNumberByIndex(int i){return secret[i]};
+        void play();
+        unordered_map<int, int> getCounter(){return counter};
     private:
         int guessCount;
         int size;
         vector<int> secret;
+        unordered_map<int, int> counter;
         int getRandomNumber(int range);
 };
